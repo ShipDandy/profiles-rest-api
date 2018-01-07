e56267e41f642a559e4cac68c03ee0d7250f6a2b
joe brower <zyoosun@gmail.com>
2018-01-07 15:31:32 -0600

    django apis seem needlessly complex

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -52,6 +52,8 @@ class HelloAPIView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
 
+    serializer_class = serializers.HelloSerializer
+
     def list(self, request):
         """return a hello message"""
 
@@ -60,3 +62,35 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({"message": "hello!", "viewset": a_viewset})
+
+    def create(self, request):
+        """create a new hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get("name")
+            message = "Hello {}".format(name)
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its ID."""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object."""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """handles removing an object"""
+
+        return Response({"http_method": "DELETE"})
