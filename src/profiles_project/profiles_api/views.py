d575b9cdf59fef0c8b1740a93bdaa04cadda229d
joe brower <zyoosun@gmail.com>
2018-01-07 14:29:53 -0600

    added other methods to profiles_api views

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.views import Response
+from rest_framework.views import status
+
+from . import serializers
 
 # Create your views here.
 
 class HelloAPIView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -15,3 +20,30 @@ class HelloAPIView(APIView):
         ]
 
         return Response({"message": "Hello!", "apiView": an_APIview})
+
+    def post(self, request):
+        """create a hello message with our name"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get("name")
+            message = "Hello {}".format(name)
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object"""
+
+        return Response({"Method": "put"})
+
+    def patch(self, request, pk=None):
+        """patch request only updates fields provided in request"""
+
+        return response({"method": "patch"})
+
+    def delete(self, request, pk=None):
+        """Deletes an object"""
+
+        return Response({"method": "delete"})
