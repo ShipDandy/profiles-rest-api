0e08858c7ceceb354dbd7ff2b33f177c6e8f5604
joe brower <zyoosun@gmail.com>
2018-01-07 17:09:25 -0600

    added login and token shizz

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, status, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers, models, permissions
 
@@ -104,3 +106,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name", "email",)
+
+class LoginViewSet(viewsets.ViewSet):
+    """checks email and password and returns auth token"""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """use the ObtainAuthToken APIView to validate and create a token"""
+
+        return ObtainAuthToken().post(request)
